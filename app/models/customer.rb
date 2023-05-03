c71923570eb5417bb3bf21d4a5c8bf9510e8e969
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-02 23:23:33 -0300

    feat: adiciona associações de card, city e state no model Customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,12 @@
 class Customer < ApplicationRecord
   belongs_to :user
 
+  has_many :cards, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :telephones, as: :contactable, dependent: :destroy
   has_many :addresses, as: :addressable, dependent: :destroy
-  has_many :requested_items, through: :orders, class_name: 'OrderItem', source: :items
+  has_many :requested_items, through: :orders, class_name: "OrderItem", source: :items
+
+  has_one :city, through: :address
+  has_one :state, through: :city
 end
