be18f683976ff4c78c2745b5ceb910798ebed281
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-02 23:35:01 -0300

    feat: adiciona associações das entidades filhas do model Administrator

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,3 +1,7 @@
 class Administrator < ApplicationRecord
   belongs_to :user
+
+  has_many :chefs, inverse_of: :approver, dependent: :nullify
+  has_many :coupons, inverse_of: :administrator, dependent: :nullify
+  has_many :telephones, as: :contactable, dependent: :destroy
 end
