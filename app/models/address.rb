95fc46287f00a382f3736f80defa071772f5e2e3
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-02 23:47:45 -0300

    feat: adiciona associações no model Address

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,9 @@ class Address < ApplicationRecord
   belongs_to :city
   belongs_to :addressable, polymorphic: true
 
+  has_many :customers, inverse_of: :address
+  has_many :orders, inverse_of: :address
+
   has_one :chef, inverse_of: :address
   has_one :state, through: :city
 end
