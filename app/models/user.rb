8e800214eddca81e9ee20a9665d949f035509e58
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-02 23:21:49 -0300

    feat: adiciona associações no model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :administrators, inverse_of: :user, dependent: :destroy
+  has_many :chefs, inverse_of: :user, dependent: :destroy
+  has_many :customers, inverse_of: :user, dependent: :destroy
 end
